19f964ebe4994121d9c47addc72357c0e45933ed
Alexander.Kartashov <alexander.kartashov@ispringsolutions.com>
2018-03-23 22:50:15 +0300

    доработаны тесты генератора таблицы подстановки.

--- decorator/streams/StreamsUnitTest/ReplacementTableGeneratorTest.cs
+++ decorator/streams/StreamsUnitTest/ReplacementTableGeneratorTest.cs
@@ -5,23 +5,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using streams;
+using System.Collections;
 
 namespace StreamsUnitTest
 {
 	[TestFixture]
+	[Parallelizable]
 	public class ReplacementTableGeneratorTest
 	{
 		[Test]
-		public void CompareReplacementTables()
+		[Parallelizable]
+		public void ReplacementTablesWithDiffetentKeysNotEqual([Random(1, int.MaxValue, 10000)] int i)
 		{
 			ReplacementTableGenerator generator = new ReplacementTableGenerator();
+			var table1 = generator.GenerateReplacementTable(i);
+			var table2 = generator.GenerateReplacementTable(i - 1);
+			CollectionAssert.AreNotEquivalent(table1, table2);
+		}
 
-			var table1 = generator.GenerateReplacementTable(100);
-			var table2 = generator.GenerateReplacementTable(100);
-			var table3 = generator.GenerateReplacementTable(99);
-
+		[Test]
+		[Parallelizable]
+		public void ReplacementTablesWithSameKeyAreEqual([Random(0, int.MaxValue, 10000)] int i)
+		{
+			ReplacementTableGenerator generator = new ReplacementTableGenerator();
+			var table1 = generator.GenerateReplacementTable(i);
+			var table2 = generator.GenerateReplacementTable(i);
 			CollectionAssert.AreEquivalent(table1, table2);
-			CollectionAssert.AreNotEquivalent(table1, table3);
 		}
 	}
 }
